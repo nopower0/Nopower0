e100182974fe2e0075b5d69535286b7e03b5b46a
jiajun Liu <jiajun.liu@ni.com>
2016-01-18 16:31:08 +0800

    Try customized PDQ

---
+++ CSharp/PDQ/SamplePDQ.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.HistoricalDebuggerHost;
+using System.Collections.Generic;
+
+namespace PDQ
+{
+    public class SamplePDQ : IProgrammableDataQuery
+    {
+        public object[] EntryQuery(object thisArg, object[] args)
+        {
+            return args;
+        }
+
+        public object[] ExitQuery(object returnValue)
+        {
+            return null;
+        }
+
+        public List<CollectedValueTuple> FormatCollectedValues(object[] results)
+        {
+            return new List<CollectedValueTuple>();
+        }
+
+        public string FormatLongDescription(object[] results)
+        {
+            return "Yeah";
+        }
+
+        public string FormatShortDescription(object[] results)
+        {
+            return "Cool";
+        }
+
+        public List<Location> GetAlternateLocations(object[] results)
+        {
+            return null;
+        }
+    }
+}
