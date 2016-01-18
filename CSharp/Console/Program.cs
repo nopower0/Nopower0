90ab5d4a4b109d2ab8a6d9d8decc7dfa11976132
jiajun Liu <jiajun.liu@ni.com>
2016-01-18 16:22:24 +0800

    Create Console project for most use cases

---
+++ CSharp/Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.HistoricalDebuggerHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TestPDQ;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
